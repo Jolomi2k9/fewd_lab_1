07a2893b5e0485d136647ceaaa58d2039c5cf954
Oritsejolomi Sillo <jsillo38@gmail.com>
2020-01-28 15:18:05 +0000

    ‘commit’

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 var color = 'Red';
 var number = 5;
+number = 6;
 var isTrue = true;
 
 console.log(color + number + isTrue);
+
---
+++ lab01.js
@@ -0,0 +1,27 @@
+
+var firstName = 'Jolomi';
+var favoriteColor = 'Blue';
+var favoriteFoods = 'Patries';
+var city = 'Dublin';
+
+//Displays My name, favorite color, favorite food and city in 3 separate statements.
+console.log(firstName);
+console.log(favoriteColor);
+console.log(favoriteFoods,city);
+;
+
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small.The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = ' is what we see. Rainbow colours, are made with light bending you see!';
+var one = ' Red';
+var two = ' orange';
+var three = ' yellow';
+var four = ' green';
+var five = ' blue';
+var six = ' indigo';
+var seven = ' violet';
+
+console.log(phrase+one+two+three+four+five+six+seven+endingPhrase);
+
+
+       
+
